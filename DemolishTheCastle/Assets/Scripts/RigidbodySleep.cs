17bcd43233415fe2ba26aad740c9dcf441e60e2c
Artem <artearteshev@gmail.com>
2022-10-21 00:55:24 +0300

    feat: add Castle

---
+++ DemolishTheCastle/Assets/Scripts/RigidbodySleep.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodySleep : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        
+        if (rigidbody != null) rigidbody.Sleep();
+    }
+}
