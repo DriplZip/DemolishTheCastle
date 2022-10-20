f9eee7e611ad9ab3947a835ed629c9c9a0ab8fe0
Artem <artearteshev@gmail.com>
2022-10-20 18:50:53 +0300

    feat: add follow camera

---
+++ DemolishTheCastle/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public static GameObject pointOfInterest;
+
+    private float _cameraZCoord;
+
+    private float _cameraSlowMotion = 0.05f;
+
+    private Vector2 _camPositionLim = Vector2.zero;
+
+    private void Awake()
+    {
+        _cameraZCoord = this.transform.position.z;
+    }
+
+    private void FixedUpdate()
+    {
+        if (pointOfInterest == null) return;
+
+        Vector3 destination = pointOfInterest.transform.position;
+        destination = Vector3.Lerp(transform.position, destination, _cameraSlowMotion);
+        
+        destination.x = Mathf.Max(_camPositionLim.x, destination.x);
+        destination.y = Mathf.Max(_camPositionLim.y, destination.y);
+        destination.z = _cameraZCoord;
+
+        transform.position = destination;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DemolishTheCastle/Assets/Scripts/SlingShot.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShot : MonoBehaviour
+{
+    private GameObject _launchPoint;
+    private Vector3 _launchPointPosition;
+    
+    private GameObject _projectile;
+    private Rigidbody _projectileRigidbody;
+
+    private bool _aimingState;
+
+    [Header("Set in Inspector")] 
+    public GameObject projectilePrefab;
+
+    public float velocityMulti = 8f;
+
+    private void Awake()
+    {
+        Transform launchPointTransform = transform.Find("LaunchPoint");
+        
+        _launchPoint = launchPointTransform.gameObject;
+        _launchPoint.SetActive(false);
+
+        _launchPointPosition = launchPointTransform.position;
+    }
+
+    private void OnMouseEnter()
+    {
+        _launchPoint.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        _launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        _aimingState = true;
+
+        _projectile = Instantiate(projectilePrefab);
+        _projectile.transform.position = _launchPointPosition;
+
+        _projectileRigidbody = _projectile.GetComponent<Rigidbody>();
+        _projectileRigidbody.isKinematic = true;
+    }
+
+    private void Update()
+    {
+        if(!_aimingState) return;
+
+        Vector3 mousePosition2d = Input.mousePosition;
+        mousePosition2d.z = -Camera.main.transform.position.z;
+
+        Vector3 mousePosition3d = Camera.main.ScreenToWorldPoint(mousePosition2d);
+
+        Vector3 launchPointPositionDiffWithMouse = mousePosition3d - _launchPointPosition;
+        float maxDiffMagnitude = this.GetComponent<SphereCollider>().radius;
+
+        if (launchPointPositionDiffWithMouse.magnitude > maxDiffMagnitude)
+        {
+            launchPointPositionDiffWithMouse.Normalize();
+            launchPointPositionDiffWithMouse *= maxDiffMagnitude;
+        }
+
+        Vector3 projectilePosition = _launchPointPosition + launchPointPositionDiffWithMouse;
+        _projectile.transform.position = projectilePosition;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _aimingState = false;
+            _projectileRigidbody.isKinematic = false;
+            _projectileRigidbody.velocity = -launchPointPositionDiffWithMouse * velocityMulti;
+            
+            FollowCam.pointOfInterest = _projectile;
+            
+            _projectile = null;
+        }
+    }
+}
